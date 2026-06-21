47b2422a6184d10b16f92ff8d8d0ea990c915afa
Stephanie <stephanie.galata@telus.com>
2026-06-21 00:00:44 -0700

    feat: fallow in ci

--- jest.config.js
+++ jest.config.js
@@ -9,15 +9,6 @@ module.exports = {
     '\\.(css|scss|sass)$': '<rootDir>/src/__mocks__/fileMock.js',
   },
   testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
-  collectCoverageFrom: [
-    'src/**/*.{ts,tsx}',
-    '!src/**/*.test.{ts,tsx}',
-    '!src/**/__tests__/**',
-    '!src/**/__mocks__/**',
-    '!src/setupTests.ts',
-    '!src/**/*.d.ts',
-  ],
-  coverageReporters: ['json', 'text-summary'],
   modulePathIgnorePatterns: ['<rootDir>/.rollup.cache/', '<rootDir>/dist/'],
   testPathIgnorePatterns: ['<rootDir>/.rollup.cache/', '<rootDir>/dist/'],
 };
